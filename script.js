99dd2fcb2c2b7a1654d7cff057c9255f127a2717
Javier-D'Alessandro <85589346+javier-dotcom@users.noreply.github.com>
2024-06-04 09:46:57 -0300

    1ra subida

---
+++ script.js
@@ -0,0 +1,29 @@
+const TEXT = "Te Espero";
+const LEN = TEXT.length;
+const chars = TEXT.split("");
+const DURATION = 4000;
+const Line1 = document.querySelector(".text-1");
+const Line2 = document.querySelector(".text-2");
+
+chars.forEach((char, i) => {
+  const createEle = (offset) => {
+    const el = document.createElement("span");
+    el.textContent = char;
+    el.classList.add("char");
+    el.setAttribute("data-offset", offset);
+    el.style.animationDelay = `-${i * (DURATION / LEN) - offset}ms`;
+    return el;
+  };
+  Line1.append(createEle(0));
+  Line2.append(createEle(-1 * (DURATION / 2)));
+});
+
+
+
+
+
+
+
+
+
+
